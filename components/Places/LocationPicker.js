f1925d36e35e9756cb2a2c39d8f7c5d4c662b2f6
ssupraja777 <ssupraja777@gmail.com>
2024-03-21 16:48:43 +0530

    locating users

--- components/Places/LocationPicker.js
+++ components/Places/LocationPicker.js
@@ -1,17 +1,52 @@
 import { StyleSheet, View } from "react-native";
 import OutlineButton from "../UI/OutlineButton";
 import { Colors } from "../../constants/Colors";
-
+import {
+  getCurrentPositionAsync,
+  useForegroundPermissions,
+  PermissionStatus,
+} from "expo-location";
 function LocationPicker() {
-  function getLocationHandler() {}
+  const [locationPermissionInformation, requestPermission] =
+    useForegroundPermissions();
+  async function verifyPermissions() {
+    if (
+      locationPermissionInformation.status === PermissionStatus.UNDETERMINED
+    ) {
+      const permissionRequest = await requestPermission();
+
+      return permissionRequest.granted;
+    }
+
+    if (locationPermissionInformation.status === PermissionStatus.DENIED) {
+      Alert.alert(
+        "Insufficient permissions",
+        "Please grant location permission"
+      );
+      return false;
+    }
+    return true;
+  }
+  async function getLocationHandler() {
+    const hasPermission = await verifyPermissions();
+
+    if (!hasPermission) return;
+
+    const userLocation = await getCurrentPositionAsync();
+    console.log("userLocation == ", userLocation);
+  }
 
   function pickOnMap() {}
   return (
     <View>
       <View style={styles.mapPreview}></View>
       <View style={styles.actions}>
-        <OutlineButton icon="location" onPress={getLocationHandler}>Locate User</OutlineButton>
-        <OutlineButton icon="map" onPress={pickOnMap}>Pick on Map</OutlineButton>
+        <OutlineButton icon="location" onPress={getLocationHandler}>
+          Locate User
+        </OutlineButton>
+        <OutlineButton icon="map" onPress={pickOnMap}>
+          Pick on Map
+        </OutlineButton>
       </View>
     </View>
   );
@@ -20,18 +55,18 @@ function LocationPicker() {
 export default LocationPicker;
 
 const styles = StyleSheet.create({
-    mapPreview: {
-        width: '100%',
-        height: 200,
-        marginVertical: 8,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: Colors.primary100,
-        borderRadius: 4
-    },
-    actions: {
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        alignItems: 'center'
-    }
-})
+  mapPreview: {
+    width: "100%",
+    height: 200,
+    marginVertical: 8,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: Colors.primary100,
+    borderRadius: 4,
+  },
+  actions: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+});

4110207243de080a46b12d7684aef86f670601d9
ssupraja777 <ssupraja777@gmail.com>
2024-03-21 15:41:03 +0530

    Configured camera access for ios

--- components/Places/ImagePicker.js
+++ components/Places/ImagePicker.js
@@ -1,14 +1,47 @@
-import { View, Button } from "react-native";
-import {launchCameraAsync} from "expo-image-picker"
+import { View, Button, Alert } from "react-native";
+import {
+  launchCameraAsync,
+  useCameraPermissions,
+  PermissionStatus,
+} from "expo-image-picker";
 
 function ImagePicker() {
+  const [cameraPermissionInformation, requestPermission] =
+    useCameraPermissions();
+
   async function imageHandler() {
+
+    const hasPermission = await verifyPermission();
+
+    if (!hasPermission) {
+      return;
+    }
+
+    async function verifyPermission() {
+      if (
+        cameraPermissionInformation.status === PermissionStatus.UNDETERMINED
+      ) {
+        const permissionRequest = await requestPermission();
+
+        return permissionRequest.granted;
+      }
+
+      if (cameraPermissionInformation.status === PermissionStatus.DENIED) {
+        Alert.alert(
+          "Insufficient permissions",
+          "Please grant camera permission"
+        );
+        return false;
+      }
+
+      return true;
+    }
     const image = await launchCameraAsync({
-        allowsEditing: true,
-        aspect: [16,9],
-        quality: 0.5
-    })
-    console.log(image)
+      allowsEditing: true,
+      aspect: [16, 9],
+      quality: 0.5,
+    });
+    console.log("image ", image);
   }
   return (
     <View>
--- screens/AddPlace.js
+++ screens/AddPlace.js
@@ -1,7 +1,6 @@
 import PlaceForm from "../components/Places/PlaceForm";
 
 function AddPlace() {
-    console.log("Cominf in add place")
     return <PlaceForm/>
 }
 
